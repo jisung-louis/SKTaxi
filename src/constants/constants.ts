1d2c45f0993c2b55c618a4d8cf22c2f5e1515a33
jisung-louis <khkh9357@naver.com>
2026-03-13 03:23:28 +0900

    refactor: root 상수를 shared와 taxi 경계로 정리

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,6 +7,12 @@ import {
   WINDOW_WIDTH,
 } from '@/shared/constants/layout';
 import { DEPARTMENT_OPTIONS } from '@/shared/constants/departments';
+export {
+  DEPARTURE_LOCATION,
+  DEPARTURE_OPTIONS,
+  DESTINATION_LOCATION,
+  DESTINATION_OPTIONS,
+} from '@/features/taxi/model/constants';
 
 export {
   BOTTOMSHEET_HANDLE_HEIGHT,
@@ -17,23 +23,3 @@ export {
   WINDOW_HEIGHT,
   WINDOW_WIDTH,
 };
-
-export const DEPARTURE_OPTIONS = [
-    ['명학역', '안양역', '금정역'],
-    ['범계역', '성결대학교'],
-];
-
-export const DESTINATION_OPTIONS = [
-    ['성결대학교', '안양역', '금정역'],
-    ['범계역', '명학역'],
-];
-
-export const DEPARTURE_LOCATION = [
-    [{ latitude: 37.3846167, longitude: 126.935323965 }, { latitude: 37.4016640, longitude: 126.92271684 }, { latitude: 37.371838475, longitude: 126.943650313 }],
-    [{ latitude: 37.3896500, longitude: 126.9506139 }, { latitude: 37.38131, longitude: 126.9288 }],
-];
-
-export const DESTINATION_LOCATION = [
-    [{ latitude: 37.38131, longitude: 126.9288 }, { latitude: 37.4016640, longitude: 126.92271684 }, { latitude: 37.371838475, longitude: 126.943650313 }],
-    [{ latitude: 37.3896500, longitude: 126.9506139 }, { latitude: 37.3846167, longitude: 126.935323965 }],
-];
