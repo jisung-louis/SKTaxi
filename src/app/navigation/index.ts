d0f885c38e4130d8f897fe275198ed739f28b2f9
jisung-louis <khkh9357@naver.com>
2026-03-18 12:37:57 +0900

    refactor: 레거시 토큰 파일과 orphan 파일 제거

--- src/app/navigation/index.ts
+++
@@ -1,6 +0,0 @@
-export { AppNavigation } from './AppNavigation';
-export { AuthNavigator } from './AuthNavigator';
-export { MainNavigator } from './MainNavigator';
-export { RootNavigator } from './RootNavigator';
-export { getCurrentChatRoomIdFromNavigationState, navigateToChatRoom } from './services/communityNavigation';
-export * from './types';
